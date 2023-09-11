c6fec04cd56d32a49b8bab1a38af90dbfc552115
pravesh meena <pravesh.meena2892@gmail.com>
2023-09-11 19:53:04 +0530

    Update actions.js

--- src/actions.js
+++ src/actions.js
@@ -1,15 +1,45 @@
-/*
-Here all the actions are defined.
-Example of defining an actoin is as follows
+// In your actions.js file
+export const repairAdded = (owner, model, description) => ({
+  type: 'repairAdded',
+  payload: {
+    id: Date.now(), // Generate a unique ID (you can use a library for this)
+    owner,
+    model,
+    description,
+    resolved: false, // Set resolved to false by default
+  },
+});
 
-export const repairAdded = (var1,va2) => {
-  return {
-    type: "actionType1",
-    payload: {
-      var1,
-      var2
-    }
-  }
-}
+export const repairRemoved = (id) => ({
+  type: 'repairRemoved',
+  payload: {
+    id,
+  },
+});
 
-*/
+export const repairResolved = (id) => ({
+  type: 'repairResolved',
+  payload: {
+    id,
+  },
+});
+
+export const repairUpdated = (id, owner, model, description) => ({
+  type: 'repairUpdated',
+  payload: {
+    id,
+    owner,
+    model,
+    description,
+  },
+});
+
+export const editTask = (id, owner, model, description) => ({
+  type: 'editTask',
+  payload: {
+    id,
+    owner,
+    model,
+    description,
+  },
+});
